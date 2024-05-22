8cd4fe0958062c59e5bbb9902bbf6dfd5d5fe92e
ceosmore <ceosmore@gmail.com>
2024-05-21 23:30:18 -0500

    Separation of code from tests

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def test_read_main():
     """
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": ·"Hello World"}
+    assert response.json() == {"message": "Hello World"}
 
 
 
